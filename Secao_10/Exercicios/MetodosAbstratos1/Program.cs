05297101c52b6af21cad894cdf2a1f963f7e1f97
Pedro <pedrodam99@gmail.com>
2023-01-19 11:06:32 -0300

    Exercicio 1 metodos abstratos - Area, Circulo e Retangulo

---
+++ Secao_10/Exercicios/MetodosAbstratos1/Program.cs
@@ -0,0 +1,55 @@
+﻿using MetodosAbstratos.Entities;
+using MetodosAbstratos.Entities.Enums;
+
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MetodosAbstratos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Shape> list = new List<Shape>();
+
+            Console.Write("Enter the numbers of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Shape #{i} data:");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Color: ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+
+                if (ch == 'r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new Rectangle(width, height, color));
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new Circle(radius, color));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("SHAPE AREAS: ");
+            foreach (Shape s in list)
+            {
+                Console.WriteLine(s.Area().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
