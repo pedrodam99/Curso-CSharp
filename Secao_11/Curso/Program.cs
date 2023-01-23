198fc2a9eaca4ed9a6fc455632c429a9b1d12159
Pedro <pedrodam99@gmail.com>
2023-01-23 12:33:02 -0300

    .

---
+++ Secao_11/Curso/Program.cs
@@ -0,0 +1,32 @@
+﻿using Curso.Entities;
+
+namespace Curso
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+           
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+            Console.WriteLine("Reservation: " +  reservation);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter data update to reservation:");
+            
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime newCheckIn = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime newCheckOut = DateTime.Parse(Console.ReadLine());
+        }
+    }
+}
