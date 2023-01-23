46ba81a8bcac788f3a7b763206415ebd078153f1
Pedro <pedrodam99@gmail.com>
2023-01-23 17:58:39 -0300

    Usando File e FileInfo

---
+++ Secao_13/Curso/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Curso
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sourcePath = @"\temp\file1.txt";
+            string targetPath = @"\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                // Usando a classe File
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                } 
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
