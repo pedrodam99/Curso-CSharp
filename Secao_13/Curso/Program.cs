f945e68ae833f34b0f304b5eaad08c064d68e1ec
Pedro <pedrodam99@gmail.com>
2023-01-24 14:11:11 -0300

    Usando StreamReader com File

--- Secao_13/Curso/Program.cs
+++ Secao_13/Curso/Program.cs
@@ -6,22 +6,17 @@ namespace Curso
     {
         public static void Main(string[] args)
         {
-            string sourcePath = @"\temp\file1.txt";
-            string targetPath = @"\temp\file2.txt";
-
+            string path = @"\temp\file1.txt";
             try
             {
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
-                // Usando a classe File
-                string[] lines = File.ReadAllLines(sourcePath);
-
-                foreach (string line in lines)
+                StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
                 {
+                    string line = sr.ReadLine();
                     Console.WriteLine(line);
-                } 
+                }
             }
-            catch(IOException e)
+            catch (IOException e)
             {
                 Console.WriteLine("An error ocurred");
                 Console.WriteLine(e.Message);
