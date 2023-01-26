36eb9a6304e1081a7024d35df7de7bbf38dd2df8
Pedro <pedrodam99@gmail.com>
2023-01-26 11:06:55 -0300

    Solução Ruim para o problema apresentado

---
+++ Secao_14/Curso/Entities/CarRental.cs
@@ -0,0 +1,21 @@
+namespace Curso.Entities
+{
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+
+        public CarRental() { }
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
---
+++ Secao_14/Curso/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+using Curso.Entities;
+using Curso.Services;
+
+namespace Curso
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string carName = Console.ReadLine();
+
+            Console.Write("Pickup (dd/mm/yyyy) hh:mm: ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Pickup (dd/mm/yyyy) hh:mm: ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(carName));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+            
+        }
+    }
+}
