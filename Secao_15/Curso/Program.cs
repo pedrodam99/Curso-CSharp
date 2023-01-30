cd50dc90587e335f2fd8bfdcd7957e93fc00fb1a
Pedro <pedrodam99@gmail.com>
2023-01-30 16:45:12 -0300

    Criando serviço do tipo int

---
+++ Secao_15/Curso/Program.cs
@@ -0,0 +1,24 @@
+﻿using Curso.Services;
+
+namespace  Curso
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine($"First: {printService.First()}");
+        }
+    }
+}
