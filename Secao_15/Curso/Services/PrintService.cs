ccfe6c7d87d5cba41b714f1eb2665151b1d10b78
Pedro <pedrodam99@gmail.com>
2023-01-30 17:00:34 -0300

    Utilizando generic

--- Secao_15/Curso/Services/PrintService.cs
+++ Secao_15/Curso/Services/PrintService.cs
@@ -1,11 +1,13 @@
+using System;
+
 namespace Curso.Services
 {
-    class PrintService
+    class PrintService<T>
     {
-        private int[] _values = new int[10];
+        private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(int value)
+        public void AddValue(T value)
         {
             if (_count == 10)
             {
@@ -15,7 +17,7 @@ namespace Curso.Services
             _count++;
         }
 
-        public int First()
+        public T First()
         {
             if (_count == 0)
             {
