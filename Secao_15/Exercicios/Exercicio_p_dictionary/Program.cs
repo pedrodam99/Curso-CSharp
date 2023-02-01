089a7ceedd04ff31f87bfebba6ce8d71333f43ba
Pedro <pedrodam99@gmail.com>
2023-02-01 01:14:13 -0300

    Exercicio usando Dictionary e StreamReader

---
+++ Secao_15/Exercicios/Exercicio_p_dictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercicio_p_dictionary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: "); // \temp\in.txt
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!(sr.EndOfStream))
+                    {
+                        string[] line = sr.ReadLine().Split(",");
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if (dict.ContainsKey(name))
+                        {
+                            dict[name] += votes;
+                        }
+                        else
+                        {
+                            dict[name] = votes;
+                        }
+                    }
+
+                    foreach (var item in dict)
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
