2db24b0dacc234b00fff8d021e52669a35b3ed0b
Pedro <pedrodam99@gmail.com>
2023-02-02 16:32:46 -0300

    Usando o Comparison

---
+++ Secao_16/Curso/Entities/Product.cs
@@ -0,0 +1,21 @@
+using System.Globalization;
+
+namespace Curso.Entities
+{
+    class Product
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Secao_16/Curso/Program.cs
@@ -0,0 +1,42 @@
+﻿using Curso.Entities;
+
+namespace Curso
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.0));
+            list.Add(new Product("Notebook", 1200.0));
+            list.Add(new Product("Tablet", 450.0));
+
+            // passando o metodo como parametro
+            list.Sort(CompareProduct);
+
+            // colocando o metodo dentro de uma variável
+            Comparison<Product> comp = CompareProduct;
+            list.Sort(comp);
+
+            // expressao lambda dentro de uma variavel
+            Comparison<Product> compLambda = (p1, p2) => p1.Price.CompareTo(p2.Price);
+            list.Sort(compLambda);
+            
+            //passando expressao lambda inline
+            list.Sort((p1, p2) => p1.Price.CompareTo(p2.Price));
+
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        // metodo que vai ser referenciado 
+        static int CompareProduct(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+    }
+}
