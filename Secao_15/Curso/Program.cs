d2b8337a9aa033c40855f5cf596fe9fe483d9753
Pedro <pedrodam99@gmail.com>
2023-02-08 14:18:19 -0300

    ...

--- Secao_15/Curso/Program.cs
+++ Secao_15/Curso/Program.cs
@@ -9,32 +9,18 @@ namespace Curso
     {
         public static void Main(string[] args)
         {
-            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
-            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
 
-            // union
-            SortedSet<int> c = new SortedSet<int>(a);
-            c.UnionWith(b);
-            PrintCollection(c);
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "9971233344";
+            cookies["phone"] = "9342343223";
 
-            // intersection
-            SortedSet<int> d = new SortedSet<int>(a);
-            d.IntersectWith(b);
-            PrintCollection(d);
-
-            // difference
-            SortedSet<int> e = new SortedSet<int>(a);
-            e.ExceptWith(b);
-            PrintCollection(e);
-        }
-
-        static void PrintCollection<T>(IEnumerable<T> collection)
-        {
-            foreach (T obj in collection)
-            {
-                Console.Write(obj + " ");
-            }
-            Console.WriteLine();
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+         
+            cookies.Remove("email");
+            Console.WriteLine(cookies["email"]);
         }
     }
 }
